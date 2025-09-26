511d33aaa00c7a8a8868033428a3dfc77223af7a
borisbolliet <boris.bolliet@gmail.com>
2025-09-27 00:02:52 +0100

    default

--- denario/research.py
+++ denario/research.py
@@ -13,5 +13,5 @@ class Research(BaseModel):
     """The results of the project."""
     plot_paths: List[str] = Field(default_factory=list, description="The plot paths of the project")
     """The plot paths of the project."""
-    keywords: Dict[str, str] = Field(default_factory=dict, description="The AAS keywords describing the project")
-    """The AAS keywords describing the project."""
+    keywords: Dict[str, str] = Field(default_factory=dict, description="The keywords describing the project")
+    """The keywords describing the project."""
